56602e1056ffe2ad89c99b95715a39850291c0d0
Diego Cepeda <dcepeda@linkedin.com>
2024-06-10 22:51:00 -0600

    just fetch

--- src/iris/api.py
+++ src/iris/api.py
@@ -2360,9 +2360,11 @@ class Incident(object):
             connection.close()
 
             incident['context'] = ujson.loads(incident['context'])
+            # TODO: UNCOMMENT
             # retrieve dynamic_tracking_notification for each incident
-            # cursor.execute(incident_dynamic_tracking_notifications_query, [(incident['id'],)])
-            # dynamic_tracking_results = cursor.fetchall()
+            cursor.execute(incident_dynamic_tracking_notifications_query, [(incident['id'],)])
+            dynamic_tracking_results = cursor.fetchall()
+            print(dynamic_tracking_results)
             # incident['dynamic_tracking'] = []
             # for tracking in dynamic_tracking_results:
             #     incident['dynamic_tracking'].append(tracking)
