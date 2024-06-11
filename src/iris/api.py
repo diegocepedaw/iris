255f6d2fc08c7ea32de038578020929e50aec0a6
Diego Cepeda <dcepeda@linkedin.com>
2024-06-10 23:22:28 -0600

    wrap in try

--- src/iris/api.py
+++ src/iris/api.py
@@ -2365,14 +2365,14 @@ class Incident(object):
             incident['dynamic_tracking'] = []
             try:
                 cursor.execute(incident_dynamic_tracking_notifications_query, [(incident_id,)])
-            except Exception:
+                dynamic_tracking_results = cursor.fetchall()
+
+                if dynamic_tracking_results:
+                    for tracking in dynamic_tracking_results:
+                        incident['dynamic_tracking'].append(tracking)
+            except Exception as e:
                 logger.exception('Failed to retrieve dynamic tracking notifications for incident %s', incident_id)
-            # dynamic_tracking_results = cursor.fetchall()
-            # print("aaa", dynamic_tracking_results, "aaa")
-            # if not dynamic_tracking_results:
-            #     print("poooop")
-            # for tracking in dynamic_tracking_results:
-            #     incident['dynamic_tracking'].append(tracking)
+                print(e)
             payload = ujson.dumps(incident)
         else:
             connection.close()
